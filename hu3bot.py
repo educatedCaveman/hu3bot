d43cf3b802c85daa950a7db8327e81dac697078c
drake <j.drake.hoffman@gmail.com>
2022-12-11 16:33:29 -0500

    debugging and expansion

--- hu3bot.py
+++ hu3bot.py
@@ -7,6 +7,7 @@ from dotenv import load_dotenv
 import math
 import asyncio
 from tabulate import tabulate
+import pprint
 
 # attempt to evaluate secrets either from a .env file or from docker secrets.
 # https://stackoverflow.com/questions/65447044/python-flask-application-access-to-docker-secrets-in-a-swarm/66717793#66717793
@@ -16,7 +17,23 @@ if os.path.exists(dotenv_path):
     load_dotenv(dotenv_path=dotenv_path)
 
 
-def manage_secrets(name):
+def manage_secrets(name:str):
+    """
+    will evaluate secrets from either environment variables or docker
+    secrets. (assumed to be /run/secrets/<secret_name>)
+
+    Parameters
+    -----------
+    name : str
+        name of the secret
+
+    Returns
+    -----------
+    str
+        the value of the secret
+    KeyError
+        exception 
+    """
     var1 = os.getenv(name)
     
     secret_path = f'/run/secrets/{name}'
@@ -49,6 +66,9 @@ bot = commands.Bot(command_prefix='!', intents=bot_intents)
 
 @bot.event
 async def on_ready():
+    """
+    runs when the bot is ready
+    """
     print(f'hu3bot is ready!')
     # print(DISCORD_TOKEN)
     #TODO: put a constant check of the printer status here?
@@ -72,7 +92,7 @@ def capture_snapshot(cam='main'):
         URL = f"http://{PRINTER_HOST}:{CAM_PORT_MAIN}/?action=snapshot"
     else:
         URL = f"http://{PRINTER_HOST}:{CAM_PORT_ALT}/?action=snapshot"
-    snapshot = f"/tmp/snapshot_{datetime.datetime.now().strftime('%F-%T')}.jpeg"
+    snapshot = f"/tmp/snapshot_{datetime.datetime.now().strftime('%F-%T.%f')}.jpeg"
     print(f'capturing snapshot: {snapshot}')
     response = requests.get(URL)
     open(snapshot, 'wb').write(response.content)
@@ -81,6 +101,19 @@ def capture_snapshot(cam='main'):
 
 
 def to_lower(arg):
+    """
+    wrapper for making a string all lower-case
+
+    Parameters
+    -----------
+    arg : str
+        text to lower-case
+
+    Returns
+    -----------
+    str
+        input text in lower-case
+    """
     return arg.lower()
 
 
@@ -114,7 +147,7 @@ async def snapshot(context, *, cam:to_lower='main'):
             embed.set_image(url=f"attachment://{base_name}")
             embeds.append(embed)
             # need to sleep so the file names are different
-            await asyncio.sleep(1)
+            # await asyncio.sleep(1)
 
         await context.send(files=files)
 
@@ -123,67 +156,115 @@ async def snapshot(context, *, cam:to_lower='main'):
         await context.send(message)
 
 
-# @bot.command(name='test')
-# async def test(context):
-#     """
-#     basic test/debugging command
-#     """
-#     await context.send(content="I'm not quite dead yet!  I don't want to go on the cart!")
+async def catch_moonraker_error(context, resp):
+    """
+    checks for a successful moonraker API call, and sends a message
+    on failure, describing said failure.
+
+    Parameters
+    -----------
+    context:
+        bot context
+    resp:
+        the requests.Response() object
+
+    Returns
+    -----------
+    bool
+        was the request to moonraker successful?
+    """
+    if isinstance(resp, requests.models.Response):
+        # await context.send('request is valid')
+        return False
+    else:
+        if isinstance(resp, requests.exceptions.Timeout):
+            await context.send(f'response is a timeout exception:\n```{resp}```')
+        if isinstance(resp, requests.exceptions.TooManyRedirects):
+            await context.send(f'response is a redirect exception:\n```{resp}```')
+        if isinstance(resp, requests.exceptions.RequestException):
+            await context.send(f'response is a request exception:\n```{resp}```')
+        else:
+            await context.send(f'respnse is something else:\n```{resp}```')
+
+        return True
 
 
+def get_from_moonraker(api:str=None):
+    """
+    performs an API call to moonraker
 
-async def get_from_moonraker(context, api:str=None):
+    Parameters
+    -----------
+    api : str , optional
+        the api object to query
+
+    Returns
+    -----------
+    requests.Response()
+        the response to the request
+    Exception
+        any exception thrown during the API call
+    """
     #check for nothing being passed:
     if api is None:
         return api
 
-    URL = f"http://{PRINTER_HOST}:{MOONRAKER_API_PORT}/{api}"
-
     try:
+        URL = f"http://{PRINTER_HOST}:{MOONRAKER_API_PORT}/{api}"
         response = requests.get(URL)
-
-    except requests.exceptions.Timeout as e:
-        msg = """Moonraker timeout. Please investigate:        
-        ```{e}```
-        """
-        await context.send(msg)
-
-    except requests.exceptions.TooManyRedirects as e:
-        msg = """Too many redirects from Moonraker. Please investigate:        
-        ```{e}```
-        """
-        await context.send(msg)
-
-    except requests.exceptions.RequestException as e:
-        msg = """Unkown Moonraker error. Please investigate:        
-        ```{e}```
-        """
-        await context.send(msg)
-
-    # when the request is successful, return the data
-    data = response.json()
-    return data
+        return response
+    except Exception as e:
+        return e
 
 
 def time_fmt(secs:float):
-    hours = str(math.floor(secs / 3600))
+    """
+    formats seconds in the hh:mm format
+
+    Parameters
+    -----------
+    secs : float
+        number of seconds
+
+    Returns
+    -----------
+    str
+        formatted time
+    """
+    hours = math.floor(secs / 3600)
+    hours_fmt = "{:,}".format(hours)
     mins = str(math.floor((secs % 3600) / 60)).zfill(2)
-    return f"{hours}h:{mins}m"
+    return f"{hours_fmt}h : {mins}m"
 
 
 @bot.command(name='status')
 async def status(context, *, stus:to_lower=None):
+    """
+    printer status
+
+    Parameters
+    -----------
+    context :
+        the event context
+    stus : str, optional
+        the type of status to return. can be missing or "detailed"
+
+    """
     # https://moonraker.readthedocs.io/en/latest/web_api/#printer-status
     # basic print stats
 
     print_stats_api = 'printer/objects/query?print_stats'
-    api_resp = await get_from_moonraker(context, print_stats_api)
-    print_stats = api_resp['result']['status']['print_stats']
+    api_resp = get_from_moonraker(print_stats_api)
+    if await catch_moonraker_error(context=context, resp=api_resp):
+        return
+    print_stats = api_resp.json()['result']['status']['print_stats']
     state = print_stats['state']
 
     display_stats_api = 'printer/objects/query?display_status'
-    api_resp = await get_from_moonraker(context, display_stats_api)
-    display_stats = api_resp['result']['status']['display_status']
+    api_resp = get_from_moonraker(display_stats_api)
+    if await catch_moonraker_error(context=context, resp=api_resp):
+        return
+    display_stats = api_resp.json()['result']['status']['display_status']
     progress = display_stats['progress']
     prog_pct = round(progress * 100, 1)
 
@@ -237,84 +318,126 @@ async def status(context, *, stus:to_lower=None):
         await context.send(message)
 
 
-#TODO: reset the stats
+def split_string(line:str, max_len:int=70):
+    """
+    splits a string into a list of shorter strings
+
+    The input string is split via spaces, and said space is removed.
+    The space chosen is the last one in the first <max_len> chars in
+    the text still needing to be split.
+
+    Parameters
+    -----------
+    line : str
+        the string to be split over multiple lines
+    max_len : int , optional
+        the maximum length of the lines being split. defaults to 70
+
+    Returns
+    -----------
+    list
+        input string split into "lines"
+    """
+    if len(line) <= max_len:
+        tmp_list = [line.strip()]
+        return tmp_list
+
+    else:
+        all_indexes = [x for x, v in enumerate(line) if v == ' ']
+        short_indexes = list(filter(lambda index: index <= max_len, all_indexes))
+        max_space = max(short_indexes)
+        short_line = [line[:max_space]]
+        remainder = line[max_space:].strip()
+        short_line.extend(split_string(remainder))
+        return short_line
 
 
-def split_lines(line:str, max_len:int):
-    too_long = True
-    new_lines = []
-    tmp_val = line
-    while too_long:
-        # make sure the first character isn't a space:
-        tmp_val = tmp_val.strip()
+def dict_to_table(json_dict:dict, max_len:int=70, compact:bool=False, last:bool=False):
+    """
+    converts a dictionary/json into a formatted table for discord
 
-        # the n-th character happens to be a space
-        if tmp_val[max_len] in (' '):
-            new_lines.append(tmp_val[:max_len])
-            tmp_val = tmp_val[:max_len]
-        else:
-            end_space = False
-            new_str = tmp_val[:max_len]
-            index = max_len-1
+    Parameters
+    -----------
+    json_dict : dict
+        the input data to format
+    max_len : int , optional
+        the maximum length of the formatted values
+    compact : bool , optional
+        print the values in a compact manner of not
+    last : bool , optional
+        continue recursing or not.
 
-            # find a space < max_len
-            while not end_space:
-                # index += -1
-                if new_str[index] in (' '):
-                    end_space = True
-                    length = index + 1
-                    row_to_append = new_str[:length].strip()
-                    print(row_to_append)
-                    new_lines.append(row_to_append)
-                    tmp_val = new_str[length:]
-                    print(tmp_val)
-
-                    # handle the remaining data not being too long
-                    if len(tmp_val) <= max_len:
-                        print('last line!')
-                        new_lines.append(tmp_val)
-                        too_long = False
-                index += -1
-            
-
-        # check if the new value to split is > max_len
-        if len(tmp_val) > max_len:
-            too_long = False
-
-    # return the formatted data    
-    print(len(new_lines))
-    print(new_lines)       
-    return "\n".join(new_lines)
-
-
-def dict_to_table(json_dict:dict, max_len:int=70):
+    Returns
+    -----------
+    str
+        a tabluate table formatted into a discord message text
+    """
     table = []
-    # print(f"max length: {max_len}")
+    pp = pprint.PrettyPrinter(indent=4, compact=compact, width=max_len)
     for key in json_dict:
-        # print(f"key: {key}")
         value = json_dict[key]
-        # print(f"value (original): {value}")
-        # handle when the value is a dictionary, itself and isn't just a single item
-        # print(f"len(value): {len(value)}")
-        if isinstance(value, dict) and len(value) > 1:
-            value = "{...}"
+
+        # format list-like types
+        if isinstance(value, dict) or \
+                isinstance(value, list) or \
+                isinstance(value, tuple):
+            if compact:
+                value = "[...]"
+            else:
+                value = pp.pformat(value)
+        
         # handle long value strings
-        elif len(value) > max_len:
-            value = split_lines(line=value, max_len=max_len)
-    
-        # print(f"value (new): {value}")
+        elif len(str(value)) > max_len:
+            # value isn't already a string, convert it
+            if not isinstance(value, str):
+                value = str(value)
+            value = "\n".join(split_string(line=value, max_len=max_len))
+
         table.append([key, value])
 
-    return table
+    # check length of table as a string:
+    fmt_table = f"```{tabulate(table, ['key', 'value'], tablefmt='fancy_grid')}```"
+    if len(str(fmt_table)) > 2000:
+        # rerun the function but make things compact, if we haven't already re-run it
+        if compact and last:
+            warning = "the following was truncated:\n"
+            full_message = warning + fmt_table
+            truncated = full_message[:1997] + "```"
+            return truncated
+        else:
+            fmt_table = dict_to_table(json_dict=json_dict, max_len=max_len, compact=True, last=True)
+
+    return fmt_table
 
 
 @bot.command(name='info')
 async def info(context, *, info_req:to_lower=None):
+    """
+    information about printer objects
 
+    objects can be queried by running `!info <object_name>`. Many objects
+    will have sub-sections. When possible, these will also be printed 
+    when querying top-level objects. If they cannot be printed, they will
+    be shown as `[...]`. These lower-level objects can be queried by
+    running `!info <object_name>/<sub_object>`.
+
+    Parameters
+    -----------
+    context:
+        event context
+    info_req : str , optional
+        the requested information section.
+        sections.
+    """
+
+    # *SETUP
     # all printer object, with some filters
     obj_api = 'printer/objects/list'
-    api_resp = await get_from_moonraker(context, obj_api)
-    objects = api_resp['result']['objects']
+    api_resp = get_from_moonraker(api=obj_api)
+    if await catch_moonraker_error(context=context, resp=api_resp):
+        return
+    # disect the response
+    objects = api_resp.json()['result']['objects']
     obj_list = []
     omit_strings = ['gcode_macro', 'configfile', ]
     for obj in objects:
@@ -325,6 +448,7 @@ async def info(context, *, info_req:to_lower=None):
         if not skip:
             obj_list.append(obj)
 
+    # *CASE 1
     # when no argument provided
     if info_req is None:
         msg = "The following printer objects can be queried:\n\n```"
@@ -332,29 +456,96 @@ async def info(context, *, info_req:to_lower=None):
             msg += f"{obj}\n"
         msg += "```\nto query these, type `!info <item>`"
         await context.send(msg)
-
+    
+    # *CASE 2
     # some argument provided
+    # assumed to be a single request
     else:
-        args = info_req.split()
+        args = info_req.split('/')
+        arg = args[0]
 
-        #TODO: make a single API call, and disect the response?
+        # for arg in args:
+        api_path = f'printer/objects/query?{arg}'
+        api_resp = get_from_moonraker(api=api_path)
+        if await catch_moonraker_error(context=context, resp=api_resp):
+            return
 
-        headers = ['key', 'value']
-        for arg in args:
-            #TODO: handle path/to/thing
-            api_path = f'printer/objects/query?{arg}'
-            api_resp = await get_from_moonraker(context, api_path)
-            resp = dict_to_table(api_resp['result']['status'][arg])
-            resp_fmt = tabulate(resp, headers, tablefmt='fancy_grid')
-            print(resp_fmt)
-            # await context.send(f"```{resp_fmt}```")
+        tmp_json_resp = api_resp.json()['result']['status'][arg]
+        api_resp = tmp_json_resp
+        if len(args) > 1:
+            for sub_arg in args:
+                if sub_arg != arg:
+                    api_resp = api_resp[sub_arg]
 
-
-
-
-        # arg_lines = "\n".join(args)
-        # msg = f"you requested info about\n```{arg_lines}```"
-        # await context.send(msg)
+        resp = dict_to_table(api_resp)
+        await context.send(resp)
 
 
+
+#TODO: reset the stats
+@bot.command(name='history', aliases=['jobs'])
+async def history(context, *, details:to_lower=None):
+    """
+    converts a dictionary/json into a formatted table for discord
+
+    Parameters
+    -----------
+    context : 
+        the event context
+    details : str , optional
+        the sub-command. 
+    """
+
+    #*default: just some high-level history stats
+    if details is None:
+        job_hist = 'server/history/list?limit=10'
+        api_resp = get_from_moonraker(job_hist)
+        if await catch_moonraker_error(context=context, resp=api_resp):
+            return
+        hist_stats = api_resp.json()['result']['jobs']
+
+        hist_data = []
+        for job in hist_stats:
+            row = [
+                job['filename'].split('/')[-1],
+                f"{round(job['filament_used'] / 1000, 1)}m",
+                time_fmt(job['print_duration']),
+                job['status']
+            ]
+            hist_data.append(row)
+
+        headers = ['file', 'filament', 'time', 'status']
+        fmt_table = f"{tabulate(hist_data, headers, tablefmt='fancy_grid')}"
+        print(fmt_table)
+        msg = f"```{fmt_table}```"
+        await context.send(msg)
+
+    #*job totals
+    elif details.startswith('total'):
+        # reset the job totals:
+        if details.startswith('total reset'):
+            pass
+            #TODO: use the wait for response in order to confirm the reset?
+        else:
+            totals_api = "server/history/totals"
+            api_resp = get_from_moonraker(totals_api)
+            if await catch_moonraker_error(context=context, resp=api_resp):
+                return
+            job_totals = api_resp.json()['result']['job_totals']
+            filament_fmt = round(job_totals['total_filament_used'] / 1000)
+
+            totals_fmt = [
+                ['total jobs', job_totals['total_jobs']],
+                ['total job time', time_fmt(job_totals['total_time'])],
+                ['total print time', time_fmt(job_totals['total_print_time'])],
+                ['filament used', "{:,}".format(filament_fmt) + " m"],
+                ['longest job', time_fmt(job_totals['longest_job'])],
+                ['longest print', time_fmt(job_totals['longest_print'])],
+            ]
+            fmt_table = f"{tabulate(totals_fmt, ['job stat', 'value'], tablefmt='fancy_grid')}"
+            msg = f"```{fmt_table}```"
+            await context.send(msg)
+
+
+#*Run the bot
 bot.run(DISCORD_TOKEN)
--- test.py
+++ test.py
@@ -1,26 +1,3 @@
-lorem_ipsum = """Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec imperdiet velit et hendrerit efficitur. Pellentesque tristique lorem et tellus pulvinar, eget volutpat nisi accumsan. Phasellus sagittis varius ipsum id convallis. Nunc et metus quis tortor lacinia posuere eget et dolor. In ligula urna, faucibus ac facilisis eu, consectetur quis nibh. Maecenas euismod mauris eget est lacinia, sit amet interdum diam gravida. Nulla ac ornare dolor, eu rutrum magna."""
-short_text = "this is a short line of text"
+import sys
 
-
-def split_string(line:str, length:int=70):
-
-    if len(line) <= length:
-        tmp_list = [line.strip()]
-        return tmp_list
-
-    else:
-        all_indexes = [x for x, v in enumerate(line) if v == ' ']
-        short_indexes = list(filter(lambda index: index <= length, all_indexes))
-        max_space = max(short_indexes)
-        short_line = [line[:max_space]]
-        remainder = line[max_space:].strip()
-        short_line.extend(split_string(remainder))
-        return short_line
-        
-
-# fmt = split_string(line=lorem_ipsum, length=70)
-fmt = split_string(line=short_text, length=70)
-
-print(fmt)
-# for line in fmt:
-#     print(line)
+print(len(sys.argv))
